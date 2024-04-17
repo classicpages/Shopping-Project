17b8e612e3a7d744e7ec5bdcf9b1bd906de2b75d
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-04-17 11:33:41 +0000

    Intial Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    This is home page
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
